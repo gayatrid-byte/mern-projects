9216e285e5ad1e66c26c401ed24b4650764c725c
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-05-15 09:33:58 +0530

    Update db.js

--- TaskManagementApp/backend/config/db.js
+++ TaskManagementApp/backend/config/db.js
@@ -11,3 +11,30 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
+
+// npm uninstall mongodb mongoose
+//npm install mongoose@7 mongodb@6
+//npm run dev
+
+
+// npm run dev -- --host
+// curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+// source ~/.bashrc
+//nvm install 20
+//[ec2-user@ip-172-31-23-182 frontend]$ node -v
+//v20.20.2
+//[ec2-user@ip-172-31-23-182 frontend]$ npm install
+//npm run dev -- --host
