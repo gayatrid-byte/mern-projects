dcb058ec7c14aee3894fc8eb377ee6fc05fbf481
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-05-15 16:55:33 +0530

    Update db.js

--- StudentRecordManagementApp/backend/config/db.js
+++ StudentRecordManagementApp/backend/config/db.js
@@ -11,3 +11,17 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// npm uninstall mongodb mongoose
+//npm install mongoose@7 mongodb@6
+//npm run dev
+
+
+// npm run dev -- --host
+// curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+// source ~/.bashrc
+//nvm install 20
+//[ec2-user@ip-172-31-23-182 frontend]$ node -v
+//v20.20.2
+//[ec2-user@ip-172-31-23-182 frontend]$ npm install
+//npm run dev -- --host
